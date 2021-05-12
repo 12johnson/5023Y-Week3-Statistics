99d6762ff56c75cb073e5c8ceb5978013c21b82f
Aaron Johnson <gfu19wzu@uea.ac.uk>
2021-05-12 15:27:30 +0000

    plots showing trends in actual data, predicted and residuals

--- week3.R
+++ week3.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(skimr)
+library(patchwork)
 
 wood_density <- read_csv("Data/wood_density.csv")
 
@@ -25,15 +26,44 @@ density_model
 ### creates a model, with Hardness as the dependent and Density as independent variable
 ### Hardness = -1160.50 + 57.51(Density)
 
+### -1160.50 + (57.51*24.7) =259.997
+### manual calculation of the lowest Hardness
 
+### coef(density_model)[1] + coef(density_model)[2]*24.7
+### This code calculates the hardness using the coefficients from the model
+### the result is 259.9152, this is lower as the coefficients have not been rounded
 
+fitted(density_model)
 
+### this code shows the predicted Hardness for the Density's of wood in the data frame
+### we can now work out the residuals from the actual Hardness minus the predicted Hardness
+### example: 484-259.9152 = 224.0848
 
+wood_density_augmented <- wood_density %>% 
+  mutate(predictions=fitted(density_model)) %>% 
+  mutate(residuals=Hardness-predictions)
 
+### this code adds two columns
+### the first being the predicted Hardness
+### the second being the residual (difference between prediction and the actual Hardness)
 
+p1 <- wood_density_augmented %>% 
+  ggplot(aes(x=Density, y=Hardness))+
+  geom_line()+
+  ggtitle("Full Data")
 
+p2 <- wood_density_augmented %>% 
+  ggplot(aes(x=Density, y=predictions))+
+  geom_line()+
+  ggtitle("Linear trend")
 
+p3 <- wood_density_augmented %>% 
+  ggplot(aes(x=Density, y=residuals))+
+  geom_hline(yintercept=0, colour="white", size=5)+
+  geom_line()+
+  ggtitle("Remaining pattern")
 
+p1+p2+p3
 
-
+### adding all 3 plots together to visually see it.
 
